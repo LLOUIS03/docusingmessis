5859c0a39cda9a918491a49a9e26da2d594a0fc3
Luis Louis <luis.louis@bairesdev.com>
2022-06-09 10:46:13 -0400

    Access Token Generated with JWT with expiration at 2030

--- src/docuSign/sendEnvelope.js
+++ src/docuSign/sendEnvelope.js
@@ -2,25 +2,18 @@ const docusign = require('docusign-esign');
 const { makeEnvelope } = require('./makeEnvelope');
 
 async function sendEnvelope(args) {
-  // Data for this method
-  // args.basePath
-  // args.accessToken
-  // args.accountId
-
   let dsApiClient = new docusign.ApiClient();
+  dsApiClient.setOAuthBasePath(args.oAuthBasePath);//development environment path
   dsApiClient.setBasePath(args.basePath);
   dsApiClient.addDefaultHeader("Authorization", "Bearer " + args.accessToken);
-  let envelopesApi = new docusign.EnvelopesApi(dsApiClient),
-    results = null;
+
+  let envelopesApi = new docusign.EnvelopesApi(dsApiClient)
 
   // Step 1. Make the envelope request body
   let envelope = makeEnvelope(args.envelopeArgs);
-  console.log('envelope', envelope);
+
   // Step 2. call Envelopes::create API method
   // Exceptions will be caught by the calling function
-  // results = await envelopesApi.createEnvelope(args.accountId, {
-  //   envelopeDefinition: envelope,
-  // });
 
   envelopesApi.createEnvelope(args.accountId, {
     envelopeDefinition: envelope,
