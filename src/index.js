aab7c0f3758fc1c291e1e6131cd29ced19be6daf
Luis Louis <luis.louis@bairesdev.com>
2022-06-09 11:01:41 -0400

    Access Token Generated with JWT with expiration at 2030

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const sendEnvelope = require('./docuSign/sendEnvelope');
 const { document1 } = require('./docuSign/makeEnvelope');
-const { accessToken, basePath } = require('./config/appsettings.json');
+const { accessToken, basePath, accountId, signerEmail, signerName, ccEmail, ccName } = require('./config/appsettings.json');
 
 const hostname = '127.0.0.1';
 const port = 4000;
@@ -10,15 +10,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const signerEmail = 'stevenbdl30@gmail.com';
-  const signerName = 'Steven Beltran';
-  const ccEmail = 'luis.louis@bairesdev.com';
-  const ccName = 'Luis Louis';
   const envelopeArgs = { signerEmail, signerName, ccEmail, ccName, status: 'sent' };
 
   sendEnvelope({
     envelopeArgs,
-    accountId: 'd05d0acc-e8ce-481a-945e-37dfb46864ae',
+    accountId: accountId,
     basePath: 'https://demo.docusign.net/restapi',
     oAuthBasePath: basePath,
     accessToken: accessToken
